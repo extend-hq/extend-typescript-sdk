67c86dc67173de0ac3684ef9d53e7a1f6b9aafa5
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-13 21:32:49 +0000

    Release 0.0.37

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * as Extend from "./api";
-// export { ExtendClient } from "./Client";
-export { ExtendClient } from "./wrapper/ExtendClient";
+export { ExtendClient } from "./Client";
 export { ExtendEnvironment } from "./environments";
 export { ExtendError, ExtendTimeoutError } from "./errors";
 export * as serialization from "./serialization";
